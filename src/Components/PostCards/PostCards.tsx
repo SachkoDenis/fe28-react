979f8b69b7cda486f93380e6beb5757e80151bfc
SachkoDenis <sachkodenis@gmail.com>
2022-08-31 23:03:05 +0300

    upgrade Cards

--- src/Components/PostCards/PostCards.tsx
+++ src/Components/PostCards/PostCards.tsx
@@ -1,47 +1,61 @@
 import React from "react";
 //@ts-ignore
 import styles from './PostCards.module.css'
-import { PostCardsPropsType } from './types'
+import classNames from 'classnames'
+import { IconBookmark } from "../../Assets/Icons/IconBookmark";
+import { IconMoreHorizontal } from "../../Assets/Icons/IconMoreHorizontal";
+import { IconThumbsDown } from "../../Assets/Icons/IconThumbsDown";
+import { IconThumbsUp } from "../../Assets/Icons/IconThumbsUp";
 
-const posts = () => {
 
-const POSTS_NAME = [
-    {
-        id: 0,
-        image: "string",
-        text: "string",
-        date: "2022-08-23",
-        lesson_num: 0,
-        title: "string",
-        author: 0
-      }
-]
-return (
-  posts.map(posts) => (
-  <div className={styles.postcard}>
-    <div className={styles.postcard_content}>
-      <div className={styles.postcard_content_left}>
-        <div className={styles.postcard_date}> {posts.date} </div>
-        <h2>{posts.title}</h2>
-        <p>{posts.text}</p>
-      </div>
-      <div className={styles.postcard_content_right}>
-        <div>{posts.image}</div>
-      </div>
-      <div className={styles.postcard_content_down}>
-        <div className={styles.postcard_content_down_left}>
-          <img src="" alt="" />
-          
-          <img src="" alt="" />
-        </div>
-        <div className={styles.postcard_content_down_right}>
-          <img src="" alt="" />
-          <img src="" alt="" />
-        </div>
-      </div>
-    </div>
-  </div>
-))
+export enum CardSize {
+    Large = 'large',
+    Medium = 'medium',
+    Small = 'small'
 }
+const PostCard = ({ post, size }: any) => {
+  const { id, image, text, date, title  } = post;
 
-export default posts
+  return (
+    <div
+      className={classNames(styles.post, {
+        [styles.largePost]: size === CardSize.Large,
+        [styles.mediumPost]: size === CardSize.Medium,
+        [styles.smallPost]: size === CardSize.Small,
+      })}
+    >
+      <div className={styles.postCard}>
+          <div className={styles.postCardContent}>
+          <div className={styles.postCardContentLeft}>
+            <div className={styles.postCardDate}> {date} </div>
+            <div className={styles.postCardTitle}> {title}</div>
+            {size === CardSize.Large && (
+            <div className={styles.postCardText}> {text}</div>
+            )}
+          </div>
+          <div className={styles.postCardContentRight}>
+            <div className={styles.postCardContentImage}>
+              <img src={image} alt="img" /> 
+            </div>
+          </div>
+          </div>
+          <div className={styles.postCardContentIcons}>
+            <div className={styles.postCardContentIconsLeft}>
+              <IconThumbsUp />
+              <IconThumbsDown />
+            </div>
+            <div className={styles.postCardContentIconsRight}>
+              <IconBookmark/>
+              <IconMoreHorizontal />
+            </div>
+          </div>
+        </div>
+  </div>
+  );
+};
+
+
+  
+
+
+export default PostCard
